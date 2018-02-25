38d83cb8688d482ad23aa79afa9dacd31680be00
Adam Jędrych <adam.jedrych25@gmail.com>
2018-02-25 12:48:11 +0100

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-project_dir=$(pwd)
+FOLDER=$(pwd)
 
-echo "so ${project_dir}/vimrc.vim" > ~/.vimrc
-echo "so ${project_dir}/tmux.conf" > ~/.tmux.conf
+if [ -d "$FOLDER/.git" ]; then
+    rm ~/.vimrc
+    ln -s ${FOLDER}/vimrc.vim ~/.vimrc
 
-# create .vim dir if it doesn't exist
-if [ ! -d "~/.vim" ]; then
-    mkdir ~/.vim
+    rm ~/.tmux.conf
+    ln -s ${FOLDER}/tmux.conf ~/.tmux.conf
+
+    rm ~/.gitconfig
+        ln -s ${FOLDER}/gitconfig ~/.gitconfig
+
+    # create .vim dir if it doesn't exist
+    if [ ! -d "~/.vim" ]; then
+        mkdir ~/.vim
+    fi
+    # create .vim/bundle if it doesn't exist
+    if [ ! -d "~/.vim/bundle" ]; then
+        mkdir ~/.vim/bundle
+    fi
+
+    # download vundle if it doesn't exist
+    vundle_dir="~/.vim/bundle/Vundle.vim"
+    if [ ! -d "${vundle_dir}" ]; then
+        git clone https://github.com/VundleVim/Vundle.vim.git ${vundle_dir}
+    else
+        echo "vundle is already installed"
+    fi
+
+    # update and install all vim plugins
+    vim +PluginClean! +PluginUpdate +PluginInstall +qall
+    python3 ~/.vim/bundle/youcompleteme/install.py --clang-completer
+else
+    echo "wrong directory :("
 fi
-
-# create .vim/bundle if it doesn't exist
-if [ ! -d "~/.vim/bundle" ]; then
-    mkdir ~/.vim/bundle
-fi
-
-# download vundle if it doesn't exist
-vundle_dir="~/.vim/bundle/Vundle.vim"
-if [ ! -d "${vundle_dir}" ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ${vundle_dir}
-fi
-
-# update and install all vim plugins
-vim +PluginClean! +PluginUpdate +PluginInstall +qall
-python3 ~/.vim/bundle/youcompleteme/install.py --clang-completer
