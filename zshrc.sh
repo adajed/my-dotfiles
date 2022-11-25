3366e9be862235d2a1664182f40a365de7f61baa
Adam Jędrych <adam.jedrych25@gmail.com>
2022-11-25 14:29:36 +0100

    [zsh] Update theme and plugins

--- zshrc.sh
+++ zshrc.sh
@@ -8,7 +8,7 @@ export ZSH="${HOME}/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="dstufft"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
@@ -57,11 +57,13 @@ COMPLETION_WAITING_DOTS="true"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
-  git
   colored-man-pages
+  docker
+  tmux
+  ubuntu
   zsh-syntax-highlighting
   zsh-autosuggestions
-  tmux
+  vi-mode
 )
 
 source $ZSH/oh-my-zsh.sh
